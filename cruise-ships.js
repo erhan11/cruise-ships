42e26346658dbcc28c11bd055b24eb1e5189efa5
SwordFish7249 <cem.gokdel@ktbmo.org>
2021-07-14 18:08:12 +0300

    Tests for Ship and Ship constructor

--- cruise-ships.js
+++ cruise-ships.js
@@ -1,8 +1,8 @@
-function Ship(Object, ship){
+function Ship(startingPort){
     this.height = 30;
     this.width = 15;
     this.long = 30;
-    this.ship = startingPort;
+    this.startingPort = startingPort;
     //const area = height, width, long;
     
     
